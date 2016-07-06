6a9348b2317baadae30bf571600a6b92e38e8b9d
Harley Montgomery <harleymontgomery@gmail.com>
2016-07-06 13:50:16 -0700

    fix analyze.py

--- analyze.py
+++ analyze.py
@@ -15,7 +15,7 @@ labels = ['BADMM (Off Policy)', 'Off Policy, Classic Step', 'Off Policy, Global 
 seeds = [0, 1, 2]
 colors = ['k', 'r', 'm', 'b', 'g']
 
-#SUCCESS_THRESHOLD = -0.5 + 0.06
+SUCCESS_THRESHOLD = -0.5 + 0.06
 
 def pickle_final_eepts(task, expt, seed, itr):
     print "Pickling task %s, expt %s, seed %s, itr %s" % (task, expt, seed, itr)
@@ -26,10 +26,10 @@ def pickle_final_eepts(task, expt, seed, itr):
 
     with open(fname, 'rb') as f:
         sample_lists = cPickle.load(f) # (num_samples, M)
-        sample_list = sample_lists[0] # There's only one policy sample
 
     # magic final_eepts extraction (get first final_eepts position of eepts)
-    final_eepts = np.array([s._data[3][-1] for s in sample_list]) # (M, num_eepts)
+    # NOTE: There's only one sample per sample_list, since it's policy samples
+    final_eepts = np.array([s[0]._data[3][-1] for s in sample_lists]) # (M, num_eepts)
 
     # save final_eepts for faster replotting
     fname = "%s/final_eepts_itr_%02d.pkl" % (dirname, itr)
@@ -65,31 +65,40 @@ def get_final_eepts(task, expt, itr):
 
         pts = unpickle_final_eepts(task, expt, seed, itr)
         if np.isnan(pts).any() or np.isinf(pts).any():
-            debug_here()
+            #debug_here()
             print "Skipping, NaN/Inf, task %s, expt %s, seed %s, itr %s" % (task, expt, seed, itr)
             continue
         eepts.append(pts)
 
     return np.array(eepts) # (num_seeds, M, num_eepts)
 
-#def write_success_pcts(task, fname):
-#    with open(fname, 'w') as f:
-#        f.write('\hline\n')
-#        f.write('Iteration & ' + ' & '.join(labels) + '\\\\\n')
-#        f.write('\hline\n')
-#        for i in [3, 6, 9, 12]:
-#            line = "%d" % i
-#            for expt in expts:
-#                eepts = get_final_eepts(task, expt, i-1)
-#                zs = eepts[:, :, 2]
-#                pct = 100*np.mean(zs < SUCCESS_THRESHOLD)
-#                line += ("& %.2f" % pct) #+ "~\% "
-#            line += "\\\\ \n"
-#            f.write(line)
-#        f.write('\hline\n')
-#
-#write_success_pcts('peg', 'peg_success.txt')
-#write_success_pcts('peg_blind_big', 'peg_blind_big_success.txt')
+#########################
+## Success Percentages ##
+#########################
+def write_success_pcts(task):
+    fname = 'experiments/%s/success.txt' % task
+    with open(fname, 'w') as f:
+        f.write('\hline\n')
+        f.write('Iteration & ' + ' & '.join(labels) + '\\\\\n')
+        f.write('\hline\n')
+        for i in [3, 6, 9, 12]:
+            line = "%d" % i
+            for expt in expts:
+                eepts = get_final_eepts(task, expt, i-1)
+                zs = eepts[:, :, 2]
+                pct = 100*np.mean(zs < SUCCESS_THRESHOLD)
+                line += ("& %.2f" % pct) #+ "~\% "
+            line += "\\\\ \n"
+            f.write(line)
+        f.write('\hline\n')
+
+write_success_pcts('peg4')
+write_success_pcts('peg9')
+write_success_pcts('peg4_blind')
+
+####################
+## Distance Plots ##
+####################
 
 def plot_task(task, iters, fn):
     """ Plot 'iters' vs. 'fn(eepts)' for a given task """
@@ -136,7 +145,7 @@ plt.xlabel('Iterations')
 plt.xlim((1, iters))
 plt.ylabel('Distance to Target')
 plt.ylim((0, 0.5))
-plt.savefig('peg4_distance.png')
+plt.savefig('experiments/peg4/distance.png')
 plt.clf()
 
 # peg4 (z-position)
@@ -155,7 +164,7 @@ plt.xlabel('Iterations')
 plt.xlim((1, iters))
 plt.ylabel('Z Position')
 plt.ylim((0, 0.5))
-plt.savefig('peg4_z.png')
+plt.savefig('experiments/peg4/z.png')
 plt.clf()
 
 # peg9 (distance)
@@ -174,7 +183,7 @@ plt.xlabel('Iterations')
 plt.xlim((1, iters))
 plt.ylabel('Distance to Target')
 plt.ylim((0, 0.5))
-plt.savefig('peg9_distance.png')
+plt.savefig('experiments/peg9/distance.png')
 plt.clf()
 
 # peg9 (z-position)
@@ -193,92 +202,43 @@ plt.xlabel('Iterations')
 plt.xlim((1, iters))
 plt.ylabel('Z Position')
 plt.ylim((0, 0.5))
-plt.savefig('peg9_z.png')
+plt.savefig('experiments/peg9/z.png')
 plt.clf()
 
-#plt.figure(figsize=(16,4))
-#
-#plt.subplot(131)
-#plt.title("Obstacle Navigation")
-#task = 'obstacle_course'
-#iters = 15
-#for expt, color, label in zip(expts, colors, labels):
-#    means = []
-#    stdevs = []
-#    for itr in range(iters):
-#        eepts = get_final_eepts(task, expt, itr)
-#        if eepts.shape[0] == 0: # no more afterwards
-#            break
-#        diffs = eepts - np.array([3.0, 0, 0])
-#        dists = np.sqrt(np.sum(diffs**2, axis=3))
-#
-#        # average over conditions first
-#        dists = dists.mean(axis=1)
-#        means.append(dists.mean())
-#        stdevs.append(dists.std())
-#    itrs = np.arange(len(means)) + 1
-#    plt.errorbar(itrs, means, stdevs, c=color, label=label)
-##plt.legend()
-#plt.xlabel('Iterations')
-#plt.xlim((1, iters))
-#plt.ylabel('Distance to target')
-#plt.ylim((0, 3.0))
-#
-### Peg plot
-#plt.subplot(132)
-#plt.title("Peg Insertion")
-#task = 'peg'
-#iters = 15
-#for expt, color, label in zip(expts, colors, labels):
-#    means = []
-#    stdevs = []
-#    for itr in range(iters):
-#        eepts = get_final_eepts(task, expt, itr)
-##        if eepts.shape[0] == 0: # no more afterwards
-##            break
-#
-#        # average over conditions first
-#        zs = eepts[:, :, :, 2].mean(axis=1)
-#        means.append(zs.mean() + 0.5)
-#        stdevs.append(zs.std())
-#    itrs = np.arange(len(means)) + 1
-#    plt.errorbar(itrs, means, stdevs, c=color, label=label)
-#
-#height = 0.1*np.ones(iters)
-#plt.plot(itrs, height, 'k--')
-#plt.legend()
-#plt.xlabel('Iterations')
-#plt.xlim((1, iters))
-#plt.ylabel('Distance to target')
-#plt.ylim((0, 0.5))
-#
-### Blind peg plot
-#plt.subplot(133)
-#plt.title("Blind Peg Insertion")
-#task = 'peg_blind_big'
-#iters = 15
-#for expt, color, label in zip(expts, colors, labels):
-#    means = []
-#    stdevs = []
-#    for itr in range(iters):
-#        eepts = get_final_eepts(task, expt, itr)
-##        if eepts.shape[0] == 0: # no more afterwards
-##            break
-#
-#        # average over conditions first
-#        zs = eepts[:, :, :, 2].mean(axis=1)
-#        means.append(zs.mean() + 0.5)
-#        stdevs.append(zs.std())
-#    itrs = np.arange(len(means)) + 1
-#    plt.errorbar(itrs, means, stdevs, c=color, label=label)
-#
-#height = 0.1*np.ones(iters)
-#plt.plot(itrs, height, 'k--')
-##plt.legend()
-#plt.xlabel('Iterations')
-#plt.xlim((1, iters))
-#plt.ylabel('Distance to target')
-#plt.ylim((0, 0.5))
-#
-#plt.tight_layout()
-#plt.savefig('results.png')
+# peg4_blind (distance)
+task = 'peg4_blind'
+iters = 12
+tgt = np.array([0, 0.3, -0.5])
+fn = lambda eepts: np.sqrt(np.sum((eepts[:, :, :3] - tgt)**2, axis=2))
+
+plt.title("Blind Peg Insertion (4 conditions)")
+plot_task(task, 12, fn)
+xs = np.arange(iters) + 1
+ys = 0.1*np.ones(iters)
+plt.plot(xs, ys, 'k--')
+plt.legend()
+plt.xlabel('Iterations')
+plt.xlim((1, iters))
+plt.ylabel('Distance to Target')
+plt.ylim((0, 0.5))
+plt.savefig('experiments/peg4_blind/distance.png')
+plt.clf()
+
+# peg9 (z-position)
+task = 'peg4_blind'
+iters = 12
+tgt = np.array([0, 0.3, -0.5])
+fn = lambda eepts: eepts[:, :, 2] + 0.5
+
+plt.title("Blind Peg Insertion (4 conditions)")
+plot_task(task, 12, fn)
+xs = np.arange(iters) + 1
+ys = 0.1*np.ones(iters)
+plt.plot(xs, ys, 'k--')
+plt.legend()
+plt.xlabel('Iterations')
+plt.xlim((1, iters))
+plt.ylabel('Z Position')
+plt.ylim((0, 0.5))
+plt.savefig('experiments/peg4_blind/z.png')
+plt.clf()
